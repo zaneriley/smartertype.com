95ceac57ff4ae4603c66e5f09f7da35e373c6833
Zane Riley <zaneriley@gmail.com>
2018-02-17 16:32:21 -0800

    Dry out function.

--- components/DragAndDrop/index.js
+++ components/DragAndDrop/index.js
@@ -8,12 +8,13 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { DragDropContext, DropTarget } from "react-dnd";
 import HTML5Backend, { NativeTypes } from "react-dnd-html5-backend";
-var opentype = require("opentype.js");
 import ItemTypes from "./Constants";
 import TargetBox from "./TargetBox";
 import FileList from "./FileList";
 import DemoFont from "./DemoFont";
 import { H4 } from "../Headings";
+
+const opentype = require("opentype.js");
 
 const DemoWrapper = styled.div`
   margin-top: var(--spacing-smaller);
@@ -27,6 +28,22 @@ const DemoWrapper = styled.div`
   }
 `;
 
+const getFeatureList = font => {
+  const tags = new Set();
+  const response = font.tables.gsub.features;
+
+  response.map(feature => {
+    tags.add(feature.tag);
+  });
+
+  const featureList = tags.values();
+
+  /* TODO: Figure out how to get rid of the linting error on this. */
+  for (let feature of featureList) {
+    console.log(feature);
+  }
+};
+
 @DragDropContext(HTML5Backend)
 export default class Container extends Component {
   constructor(props) {
@@ -38,37 +55,6 @@ export default class Container extends Component {
     this.state = { droppedFiles: [] };
   }
 
-  getFeatures(e) {
-    let file = e;
-
-    let font = null;
-    let reader = new FileReader();
-
-    reader.onload = function(e) {
-      try {
-        font = opentype.parse(e.target.result);
-        console.log(e.target.result);
-
-        const tags = new Set();
-        const response = font.tables.gsub.features;
-
-        response.forEach(item => {
-          tags.add(item.tag);
-        });
-
-        for (let item of tags.values()) {
-          console.log(item);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    reader.onerror = function(err) {
-      console.log(err);
-    };
-  }
-
   handleFileDrop(item, monitor) {
     if (monitor) {
       if (monitor.getItem().filetype === "demofont") {
@@ -78,27 +64,15 @@ export default class Container extends Component {
       const droppedFiles = monitor.getItem().files;
       this.setState({ droppedFiles });
 
-      this.getFeatures(droppedFiles[0]);
-
-      let file = droppedFiles[0];
+      const file = droppedFiles[0];
 
       let font = null;
-      let reader = new FileReader();
+      const reader = new FileReader();
 
       reader.onload = function(droppedFiles) {
         try {
           font = opentype.parse(droppedFiles.target.result);
-          console.log(font);
-          const tags = new Set();
-          const response = font.tables.gsub.features;
-
-          response.forEach(item => {
-            tags.add(item.tag);
-          });
-
-          for (let item of tags.values()) {
-            console.log(item);
-          }
+          getFeatureList(font);
         } catch (err) {
           console.log(err);
         }
@@ -113,25 +87,17 @@ export default class Container extends Component {
   }
 
   handleFileUpload(e) {
-    let file = e.target.files[0];
+    const file = e.target.files[0];
 
     let font = null;
-    let reader = new FileReader();
+    const reader = new FileReader();
 
+    /* eslint-disable func-names, no-shadow */
     reader.onload = function(e) {
+      /* eslint-enable */
       try {
         font = opentype.parse(e.target.result);
-        console.log(font);
-        const tags = new Set();
-        const response = font.tables.gsub.features;
-
-        response.forEach(item => {
-          tags.add(item.tag);
-        });
-
-        for (let item of tags.values()) {
-          console.log(item);
-        }
+        getFeatureList(font);
       } catch (err) {
         console.log(err);
       }
@@ -159,13 +125,13 @@ export default class Container extends Component {
         <H4 color="var(--color-neutral-dark)">Try these fonts out</H4>
         <DemoWrapper>
           <DemoFont
-            hideSourceOnDrag={true}
+            hideSourceOnDrag
             file="http://localhost:3000/static/fonts/source-sans-pro-regular.woff"
             filename="Source Serif Pro Bold"
             filetype="demofont"
           />
           <DemoFont
-            hideSourceOnDrag={true}
+            hideSourceOnDrag
             file="http://localhost:3000/static/fonts/source-sans-pro-regular.woff"
             filename="Fira Mono Regular"
             filetype="demofont"
